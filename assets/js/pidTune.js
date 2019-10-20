e67a6fa7a637ebd01d7bed39860be49c183815b9
Chris Gerth <chrisgerth010592@gmail.com>
2019-10-19 19:56:50 -0500

    Committing build in prep for deployment

--- assets/js/pidTune.js
+++ assets/js/pidTune.js
@@ -1,3 +1,13 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// Global constatns the user will manipulate
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+FGain = 0;
+PGain = 0;
+DGain = 0;
+IGain = 0;
+setpoint = 0;
+injectBall = false;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 // Standard Math Function impelmentations
@@ -67,7 +77,7 @@ class DualPlot{
               graphType: 'polyline'
           },
           {
-            fn: '1000',
+            fn: setpoint.toString(),
             title: 'Setpoint'
           }
         ]
@@ -104,7 +114,7 @@ var GEARBOX_RATIO = 50.0/10.0; //output over input - 5:1 gear ratio
 var Rc = 0.08; //Coil & Wiring Resistance in Ohms
 var Kt = 0.71/134; //Nm/A torque constant -  Calculated from Stall Torque/Stall Current
 var Kv = (12-(0.7*Rc))/(18730*2*3.14159/60); //V/(rad/s). Calculated from Vemf@FreeSpeed/(2pi/60*RPM@FreeSpeed). Steady-state Vemf = Vs - I@FreeSpeed*Rc, for Vs = 12
-var mass = 0.75; //shooter wheel mass in Kg
+var mass = 0.55; //shooter wheel mass in Kg
 var radius = 0.0762; //3 inch radius, converted to meters
 
 // Constants from the blog post equations
@@ -184,7 +194,7 @@ function runSimFullVoltage(){
 
 }
 
- //TODO - ACTUALLY USE CLASS REPRESNTATION TO STOP MEMORY LELAK
+setpoint = 1000
 var fullVoltPlot = new DualPlot('#plot2a', '#plot2b')
 runSimFullVoltage();
 fullVoltPlot.updatePlot(motorSpeedArray, inputVoltageArray)
@@ -236,95 +246,10 @@ fullVoltDistPlot.updatePlot(motorSpeedArray, inputVoltageArray)
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
-// Bang-bang controller - interactive sample rate
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-
-function runSimBangBang(Ts_controller){
-
-    var minTime = -1.0;
-    var maxTime = 10.0;
-    var Ts = 0.001;
-    var speedPrev = 0;
-
-    nextControllerRunTime = 0.0;
-
-    on_voltage = 12.0;
-    off_voltage = 0.0;
-
-    inVolts = 0.0;
-
-    target_speed_rpm = 1000;
-    speed_rpm = 0.0;
-
-    speed_delay_line = new DelayLine(49)
-
-    extTorqueArray = [];
-    motorSpeedArray = [];
-    inputVoltageArray = [];
-    
-    for(t = minTime; t < maxTime; t += Ts){
-
-        //Simulate friction
-        extTrq = 0.0005*speedPrev;
-        if(t > 5.0 & t < 5.1){
-            //add a short "impulse" to simulate putting a ball into the shooter
-            extTrq += 2;
-        }
-
-        meas_speed = speed_delay_line.getSample();
-
-        //Simulate Controller
-        if(t >= nextControllerRunTime && t > .0){
-
-            // Bang-bang control law
-            if(meas_speed < target_speed_rpm){
-                inVolts = on_voltage;
-            } else {
-                inVolts = off_voltage;
-            }
-
-            //Maintain separate sample rate for controller
-            nextControllerRunTime += Ts_controller;
-        }
-        
-        
-        //Simulate Plant behavior
-        speed = (Ts*C1*inVolts - Ts*C3*extTrq + speedPrev)/(1+Ts*C2);
-        speedPrev = speed;
-
-        speed_rpm = speed*60/2/3.14159
-        motorSpeedArray.push([t, speed_rpm]);
-        inputVoltageArray.push([t, inVolts]);
-
-        speed_delay_line.addSample(speed_rpm);
-    }
-
-}
-
-//add interactivity
-var sampTimeSlider = document.getElementById("sampleTime_ms");
-var sampTimeDisp   = document.getElementById("samplerate_disp");
-
-var bangBangPlot = new DualPlot('#plot3a', '#plot3b')
-
-// Update the current slider value (each time you drag the slider handle)
-sampTimeSlider.oninput = function() {
-    sampTimeDisp.innerHTML = this.value + "ms";
-    runSimBangBang(this.value/1000);
-    bangBangPlot.updatePlot(motorSpeedArray, inputVoltageArray)
-} 
-
-//Init
-sampTimeSlider.oninput()
-
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////
 // PID controller - 20 ms
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function runSimPID(F_gain, P_gain, I_gain, D_gain){
+function runSimPID(F_gain, P_gain, I_gain, D_gain, target_speed_rpm){
 
     var minTime = -1.0;
     var maxTime = 10.0;
@@ -336,7 +261,6 @@ function runSimPID(F_gain, P_gain, I_gain, D_gain){
 
     inVolts = 0.0;
 
-    target_speed_rpm = 1000;
     speed_rpm = 0.0;
 
     speed_delay_line = new DelayLine(49)
@@ -354,9 +278,9 @@ function runSimPID(F_gain, P_gain, I_gain, D_gain){
 
         //Simulate friction
         extTrq = 0.0005*speedPrev;
-        if(t > 5.0 & t < 5.1){
+        if( (t > 5.0 & t < 5.1)  & injectBall){
             //add a short "impulse" to simulate putting a ball into the shooter
-            extTrq += 2;
+            extTrq *= 30.0
         }
 
         meas_speed = speed_delay_line.getSample();
@@ -394,7 +318,7 @@ function runSimPID(F_gain, P_gain, I_gain, D_gain){
         speed = (Ts*C1*inVolts - Ts*C3*extTrq + speedPrev)/(1+Ts*C2);
         speedPrev = speed;
 
-        speed_rpm = speed*60/2/3.14159
+        speed_rpm = speed*60/2/3.14159;
         motorSpeedArray.push([t, speed_rpm]);
         inputVoltageArray.push([t, inVolts]);
 
@@ -405,45 +329,83 @@ function runSimPID(F_gain, P_gain, I_gain, D_gain){
 
 
 //add interactivity
-var FGainSlider  = document.getElementById("F_gain");
-var FGainDisplay = document.getElementById("F_gain_disp");
-var PGainSlider  = document.getElementById("P_gain");
-var PGainDisplay = document.getElementById("P_gain_disp");
-var IGainSlider  = document.getElementById("I_gain");
-var IGainDisplay = document.getElementById("I_gain_disp");
-var DGainSlider  = document.getElementById("D_gain");
-var DGainDisplay = document.getElementById("D_gain_disp");
+var setpointSlider  = document.getElementById("setpointSlider");
+setpointSlider.oninput = adjustSetpoint;
+
 
 var pidPlot = new DualPlot('#plot4a', '#plot4b')
 
-// Update the plots (each time you drag any slider handle)
-function rePlot(){
-    FGain = parseFloat(FGainSlider.value)/70000;
-    PGain = parseFloat(PGainSlider.value)/175;
-    IGain = parseFloat(IGainSlider.value)/35;
-    DGain = parseFloat(DGainSlider.value)/3500;
-
-    FGainDisplay.innerHTML = FGain;
-    PGainDisplay.innerHTML = PGain;
-    IGainDisplay.innerHTML = IGain;
-    DGainDisplay.innerHTML = DGain;
-
-    plotPIDControl(FGain, PGain, IGain, DGain);
-}
-
-function plotPIDControl(FGain, PGain, IGain, DGain){
-    runSimPID(FGain, PGain, IGain, DGain);
+function plotPIDControl(){
+    runSimPID(FGain, PGain, IGain, DGain, setpoint);
     pidPlot.updatePlot(motorSpeedArray, inputVoltageArray)
+    document.getElementById("gains").innerHTML = "<b>" + 
+                                                 "F="+FGain.toFixed(4) + " <br>" +
+                                                 "P="+PGain.toFixed(4) + " <br>" +
+                                                 "I="+IGain.toFixed(4) + " <br>" +
+                                                 "D="+DGain.toFixed(4) + " <br>" +
+                                                 "Setpoint="+setpoint.toFixed(0) + " RPM" +
+                                                 "</b>";
 }
 
+function resetPIDF(){
+    FGain = 0
+    PGain = 0;
+    DGain = 0;
+    IGain = 0;
+    setpoint = 1000;
+    plotPIDControl();
+}
 
-FGainSlider.oninput = rePlot;
-PGainSlider.oninput = rePlot;
-IGainSlider.oninput = rePlot;
-DGainSlider.oninput = rePlot;
+function adjustF(adj){
+    if(FGain == 0 & adj != 0){
+        FGain = 0.0001;
+    } else {
+        FGain *= adj;
+    }
+    plotPIDControl();
+}
 
+function adjustP(adj){
+    if(PGain == 0 & adj != 0){
+        PGain = 0.0001;
+    } else {
+        PGain *= adj;
+    }
+    PGain *= adj;
+    plotPIDControl();
+}
+
+function adjustD(adj){
+    if(DGain == 0 & adj != 0){
+        DGain = 0.0001;
+    } else {
+        DGain *= adj;
+    }
+    DGain *= adj;
+    plotPIDControl();
+}
+
+function adjustI(adj){
+    if(IGain == 0 & adj != 0){
+        IGain = 0.0001;
+    } else {
+        IGain *= adj;
+    }
+    IGain *= adj;
+    plotPIDControl();
+}
+
+function adjustSetpoint(){
+    setpoint = parseFloat(setpointSlider.value);
+    plotPIDControl();
+}
+
+function updateBallInject(){
+  var checkBox = document.getElementById("ballInject");
+
+  injectBall = checkBox.checked;
+  plotPIDControl();
+}
 
 //Init
-rePlot()
-
-
+resetPIDF();
